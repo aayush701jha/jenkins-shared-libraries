91e014e920e6e9ce60f550c9fcf049b68fda6877
Aayush <aayush.jha@kollegeapply.com>
2025-03-01 02:00:57 +0530

    Update deployApp.groovy

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -1,11 +1,11 @@
-def call(String appName, String startCmd) {
+def call(String appName) {
     script {
-        def newPort = sh(script: "if [ \"$(cat /tmp/active_port)\" = \"8001\" ]; then echo 8002; else echo 8001; fi", returnStdout: true).trim()
+        def newPort = sh(script: "if [ \"\$(cat /tmp/active_port)\" = \"8001\" ]; then echo 8002; else echo 8001; fi", returnStdout: true).trim()
 
         echo "🔄 Deploying new instance on port ${newPort}"
 
         sh """
-            pm2 start ${startCmd} --name '${appName}-${newPort}' -- --port=${newPort} || pm2 restart '${appName}-${newPort}'
+            pm2 start npm --name '${appName}-${newPort}' -- start -- --port=${newPort} || pm2 restart '${appName}-${newPort}'
             pm2 save
         """
 
