6d8b5692c93241d5597c3e4c65c3554e9fbc02dd
Aayush <aayush.jha@kollegeapply.com>
2025-03-01 01:28:50 +0530

    Create deployApp.groovy
    Deployment Syntax

---
+++ vars/deployApp.groovy
@@ -0,0 +1,10 @@
+def call(String appName) {
+    script {
+        sh "pm2 delete ${appName} || true"
+        sh """
+            pm2 start npm --name '${appName}' -- start
+            pm2 save
+        """
+        echo "✅ Deployment successful for ${appName}"
+    }
+}
