c2f2303f587343edce039174203a894e0330f2f6
Aayush <aayush.jha@kollegeapply.com>
2025-03-01 01:46:42 +0530

    Update deployApp.groovy

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -2,12 +2,12 @@ def call(String appName) {
     script {
         sh """
             if pm2 list | grep -q ${appName}; then
-                pm2 reload ${appName}
+                pm2 reload ${appName} --update-env
             else
-                pm2 start npm --name '${appName}' -- start
+                pm2 start npm --name '${appName}' -- start --watch -i max
             fi
             pm2 save
         """
-        echo "🚀 Zero-Downtime Deployment Done for '${appName}'!"
+        echo "🚀 Zero-Downtime Rolling Deployment Done for '${appName}'!"
     }
 }
