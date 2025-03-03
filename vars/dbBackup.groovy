91b057b7046fb1f750316167453afd1d63bb2875
Aayush <aayush.jha@kollegeapply.com>
2025-03-03 19:04:53 +0530

    Update dbBackup.groovy

--- vars/dbBackup.groovy
+++ vars/dbBackup.groovy
@@ -1,16 +1,9 @@
-#!/usr/bin/env groovy
-
-def call(Map params) {
-    script {
-        def timestamp = new Date().format("yyyy-MM-dd_HH-mm-ss")
-        def backupFile = "/tmp/${params.dbName}_backup_${timestamp}.sql"
-
-        sh """
-        export BACKUP_FILE="${backupFile}"
-        PGPASSWORD="${params.dbPassword}" pg_dump -h ${params.dbHost} -U ${params.dbUser} -d ${params.dbName} -p 5432 > $BACKUP_FILE
-        aws s3 cp $BACKUP_FILE s3://${params.s3Bucket}/
-        """
-
-        echo "✅ Database backup completed and uploaded to S3: ${params.s3Bucket}"
-    }
+def call(Map args = [:]) {
+    def timestamp = new Date().format("yyyy-MM-dd_HH-mm-ss", TimeZone.getTimeZone('UTC'))
+    def backupFile = "/tmp/stage_backup_${timestamp}.sql"
+    
+    sh """
+        PGPASSWORD="aayush" pg_dump -h 43.204.112.36 -U postgres -d stage_test_db -p 5432 > ${backupFile}
+        aws s3 cp ${backupFile} s3://db-backup-pipeline/
+    """
 }
