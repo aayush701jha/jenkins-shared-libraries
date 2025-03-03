3ca1521fafc8f5d6dcf0f75d9b4ce89bbc0959fb
Aayush <aayush.jha@kollegeapply.com>
2025-03-03 18:47:49 +0530

    Update dbBackup.groovy

--- vars/dbBackup.groovy
+++ vars/dbBackup.groovy
@@ -1,16 +1,16 @@
 #!/usr/bin/env groovy
 
-def call(String dbHost, String dbUser, String dbPassword, String dbName, String s3Bucket) {
+def call(Map params) {
     script {
         def timestamp = new Date().format("yyyy-MM-dd_HH-mm-ss")
-        def backupFile = "/tmp/${dbName}_backup_${timestamp}.sql"
+        def backupFile = "/tmp/${params.dbName}_backup_${timestamp}.sql"
 
         sh """
         export BACKUP_FILE="${backupFile}"
-        PGPASSWORD="${dbPassword}" pg_dump -h ${dbHost} -U ${dbUser} -d ${dbName} -p 5432 > $BACKUP_FILE
-        aws s3 cp $BACKUP_FILE s3://${s3Bucket}/
+        PGPASSWORD="${params.dbPassword}" pg_dump -h ${params.dbHost} -U ${params.dbUser} -d ${params.dbName} -p 5432 > $BACKUP_FILE
+        aws s3 cp $BACKUP_FILE s3://${params.s3Bucket}/
         """
-        
-        echo "Database backup completed and uploaded to S3 bucket: ${s3Bucket}"
+
+        echo "✅ Database backup completed and uploaded to S3: ${params.s3Bucket}"
     }
 }
