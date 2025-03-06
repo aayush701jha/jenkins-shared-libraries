8bb2be516bd15dbf87b4c11cbb1e031368ed9942
Aayush <aayush.jha@kollegeapply.com>
2025-03-06 15:29:22 +0530

    Update deployApp.groovy

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -1,10 +1,15 @@
 def call(String appName) {
     script {
-        sh "pm2 delete ${appName} || true"
         sh """
-            pm2 start npm --name '${appName}' -- start
+            if pm2 list | grep -q '${appName}'; then
+                echo "🔄 Process Exists, Reloading..."
+                pm2 reload ${appName} --update-env --wait-ready --kill-timeout 3000
+            else
+                echo "🚀 Process Not Found, Starting New One..."
+                pm2 start npm --name '${appName}' -- start
+            fi
             pm2 save
         """
-        echo "🚀 Application '${appName}' Deployed Successfully!"
+        echo "✅ Application '${appName}' Deployed Successfully!"
     }
 }
